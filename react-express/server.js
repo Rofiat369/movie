e91872726b8a30049ee530ae7fca1084f3862a13
Rofiat <rofiatolusanya12@gmail.com>
2022-03-22 09:14:43 -0400

    imported node-fetch to make fetch work on the backend

--- react-express/server.js
+++ react-express/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+import fetch from 'node-fetch';
 
 const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=d0f5f2e135336200362af8a1a73acb17')
+    .then(res => console.log(res))
+
+    // res.send('Hello World, from express');
 })
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
