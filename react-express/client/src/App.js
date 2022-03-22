d09681d131e3c04d68dcc3505bb49fbceaac3dbc
Rofiat <rofiatolusanya12@gmail.com>
2022-03-22 11:37:06 -0400

    removed unused jsx

--- react-express/client/src/App.js
+++ react-express/client/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Movies from './Components/AllMovies/Movies';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      <Movies />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
