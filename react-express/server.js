88d2363da9755fa7b83da5807ab7dc63e1ee1160
Rofiat <rofiatolusanya12@gmail.com>
2022-03-22 10:43:01 -0400

    imported axios to use with API

--- react-express/server.js
+++ react-express/server.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-import fetch from 'node-fetch';
+const axios =require('axios');
 
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=d0f5f2e135336200362af8a1a73acb17')
-    .then(res => console.log(res))
+const handleError = function (res, error){
+    if(error.response){
+        res.status(error.response.status).send(error.response.data)
+    } else {
+        res.status(400).send(error.message)
+    }
+}
 
-    // res.send('Hello World, from express');
+app.get('/', (req, res) => {
+    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=d0f5f2e135336200362af8a1a73acb17')
+    .then(function ({data}) {
+        // handle success
+        res.send(data);
+    })
+    .catch(function (error){
+        handleError(res, error)
+    })
+})
+
+app.get('/liked', (req, res) => {
+    axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=d0f5f2e135336200362af8a1a73acb17')
+    .then(function ({data}) {
+        // handle success
+        res.send(data);
+    })
+    .catch(function (error){
+        handleError(res, error)
+    })
 })
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
