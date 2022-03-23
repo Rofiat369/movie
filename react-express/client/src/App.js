63d1a2acc3a0256972c503e6975445689f629b96
Rofiat <rofiatolusanya12@gmail.com>
2022-03-23 19:11:45 -0400

    removed unused variable

--- react-express/client/src/App.js
+++ react-express/client/src/App.js
@@ -5,22 +5,9 @@ import Movies from './Components/AllMovies/Movies';
 function App() {
   return (
     <div className="App">
-
+<header className="App-header">
       <Movies />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      </header>
     </div>
   );
 }
