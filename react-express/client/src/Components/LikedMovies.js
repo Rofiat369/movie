558653426f3514d4d029146fe6a5583c1f74feeb
Rofiat <rofiatolusanya12@gmail.com>
2022-03-24 13:13:05 -0400

    Created a function for liked movies

--- react-express/client/src/Components/LikedMovies.js
+++ react-express/client/src/Components/LikedMovies.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import getLikedMovies from "./getLikedMovies";
-import "./Movies.css";
-import Navigation from "./Navigation";
+import MoviePoster from "./MoviePoster";
 
 function LikedMovies() {
-  const likedMovies = getLikedMovies();
+  const [likedMovies, setLikedMovies] = useState(getLikedMovies());
+
+  // likeMovie will add movie poster to likedMovies in localStorage
+  function likeMovie(movie) {
+    const updatedLikedMovies = [...likedMovies, movie];
+    localStorage.setItem(
+      "likedMovies",
+      JSON.stringify([...likedMovies, movie])
+    );
+    setLikedMovies(updatedLikedMovies);
+  }
+
+  // dislikeMovie will remove movie poster from likedMovies in localStorage
+  function dislikeMovie(movie) {
+    const updatedLikedMovies = likedMovies.filter((likedMovie) => {
+      return likedMovie.id !== movie.id;
+    });
+    localStorage.setItem("likedMovies", JSON.stringify(updatedLikedMovies));
+    setLikedMovies(updatedLikedMovies);
+  }
+
   return (
     <div>
-      <Navigation />
       <div className="container">
-        {likedMovies.map((movie) => {
-          const imgApi = `https://image.tmdb.org/t/p/w500`;
-
+        {/* Map through all the liked movies saved in local storage and return poster image */}
+        {likedMovies.map((likedMovie) => {
           return (
-            <figure key={movie.id} className="posterImg-wrap">
-              <img
-                src={imgApi + movie.poster_path}
-                className="poster-img"
-                alt="poster"
-              />
-              
-            </figure>
+            <MoviePoster
+              key={likedMovie.id}
+              movie={likedMovie}
+              dislikeMovie={dislikeMovie}
+              likeMovie={likeMovie}
+            />
           );
         })}
       </div>
