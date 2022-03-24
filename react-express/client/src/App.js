ba1c96d2ab0b1c3946dd8017870db0b609623e7a
Rofiat <rofiatolusanya12@gmail.com>
2022-03-24 13:11:20 -0400

    imported CSS from Movies to reduce redundancy

--- react-express/client/src/App.js
+++ react-express/client/src/App.js
@@ -1,20 +1,21 @@
 // Imports
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import "./App.css"
+import "./App.css";
+
+import "./Components/Movies.css"
 
 import Movies from "./Components/Movies";
 
 import LikedMovies from "./Components/LikedMovies";
 
+import Navigation from "./Components/Navigation";
+
 function App() {
   return (
     // Routes
     <Router>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Movies />} />
         <Route path="/liked" element={<LikedMovies />} />
