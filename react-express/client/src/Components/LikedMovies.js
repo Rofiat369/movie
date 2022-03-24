807a36e5aba914110cd41e9cac9f79dfa0db9810
Rofiat <rofiatolusanya12@gmail.com>
2022-03-23 22:40:14 -0400

    Added a liked movies component

---
+++ react-express/client/src/Components/LikedMovies.js
@@ -0,0 +1,33 @@
+import React from "react";
+import getLikedMovies from "./getLikedMovies";
+
+import "./Movies.css";
+import Navigation from "./Navigation";
+
+function LikedMovies() {
+  const likedMovies = getLikedMovies();
+
+  return (
+    <div>
+      <Navigation />
+      <div className="container">
+        {likedMovies.map((movie) => {
+          const imgApi = `https://image.tmdb.org/t/p/w500`;
+
+          return (
+            <figure key={movie.id} className="posterImg-wrap">
+              <img
+                src={imgApi + movie.poster_path}
+                className="poster-img"
+                alt="poster"
+              />
+              
+            </figure>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default LikedMovies;
