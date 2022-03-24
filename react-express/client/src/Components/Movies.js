80c9f78d5324800eee780fb8a286d1aa5dff5e51
Rofiat <rofiatolusanya12@gmail.com>
2022-03-24 13:12:18 -0400

    Added in Movie Poster as props to Movies

--- react-express/client/src/Components/Movies.js
+++ react-express/client/src/Components/Movies.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { BsFillHandThumbsDownFill } from "react-icons/bs";
-import { BsFillHandThumbsUpFill } from "react-icons/bs";
 import getLikedMovies from "./getLikedMovies";
+import MoviePoster from "./MoviePoster";
 
 import "./Movies.css";
-import Navigation from "./Navigation";
 
 function Movies() {
   const [movies, setMovies] = useState([]);
@@ -24,7 +22,10 @@ function Movies() {
   // likeMovie will add movie poster to likedMovies in localStorage
   function likeMovie(movie) {
     const updatedLikedMovies = [...likedMovies, movie];
-    localStorage.setItem('likedMovies', JSON.stringify([...likedMovies, movie]));
+    localStorage.setItem(
+      "likedMovies",
+      JSON.stringify([...likedMovies, movie])
+    );
     setLikedMovies(updatedLikedMovies);
   }
 
@@ -33,47 +34,22 @@ function Movies() {
     const updatedLikedMovies = likedMovies.filter((likedMovie) => {
       return likedMovie.id !== movie.id;
     });
-    localStorage.setItem('likedMovies', JSON.stringify(updatedLikedMovies));
+    localStorage.setItem("likedMovies", JSON.stringify(updatedLikedMovies));
     setLikedMovies(updatedLikedMovies);
   }
 
   return (
     <div>
-      <Navigation />
       <div className="container">
-        {/* Map through all the items from the api and give me the poster */}
+        {/* Map through all the movie from the api and return poster image */}
         {movies.map((movie) => {
-          const imgApi = `https://image.tmdb.org/t/p/w500`;
-
-          // Check if movie.id as an equal link to any id in the liked movies
-          const foundMovieInLikedMovies = likedMovies.find((likedMovie) => {
-            return likedMovie.id === movie.id;
-          });
-
           return (
-            <figure key={movie.id} className="posterImg-wrap">
-              <img
-                src={imgApi + movie.poster_path}
-                className="poster-img"
-                alt="poster"
-              />
-              <div className="overlay">
-                {/* if movie is alread in liked movies display thumbs down else display thumbs up */}
-                {foundMovieInLikedMovies ? (
-                  <BsFillHandThumbsDownFill
-                    size={50}
-                    className="thumbsDown thumbs"
-                    onClick={() => dislikeMovie(movie)}
-                  />
-                ) : (
-                  <BsFillHandThumbsUpFill
-                    size={50}
-                    className="thumbsUp thumbs"
-                    onClick={() => likeMovie(movie)}
-                  />
-                )}
-              </div>
-            </figure>
+            <MoviePoster
+              key={movie.id}
+              movie={movie}
+              dislikeMovie={dislikeMovie}
+              likeMovie={likeMovie}
+            />
           );
         })}
       </div>
