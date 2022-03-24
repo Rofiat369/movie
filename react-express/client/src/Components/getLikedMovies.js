0e546e4d83abb39a9d833782ff60d48e0081fb4c
Rofiat <rofiatolusanya12@gmail.com>
2022-03-24 13:10:37 -0400

    Added a function to get liked movie from local storage

--- react-express/client/src/Components/getLikedMovies.js
+++ react-express/client/src/Components/getLikedMovies.js
@@ -1,8 +1,10 @@
+
+
 function getLikedMovies() {
-    if (localStorage.likedMovies) {
-      return JSON.parse(localStorage.likedMovies)
-    }
-    return [];
+  if (localStorage.likedMovies) {
+    return JSON.parse(localStorage.likedMovies);
+  }
+  return [];
 }
 
 export default getLikedMovies;
